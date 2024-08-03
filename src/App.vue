7ab5af08b6c857fa15bb90272e37bca7960e12ff
Fernando9200 <fernando91.sosa@hotmail.com>
2024-08-02 21:43:15 -0300

    Added user logged info on top

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
       <v-spacer></v-spacer>
       <v-btn text @click="$router.push('/')">Products</v-btn>
       <v-btn text @click="$router.push('/cart')">Cart</v-btn>
-      <v-btn text v-if="!isAuthenticated" @click="$router.push('/register')">Register</v-btn>
+      <!-- Verifica se o usuário está logado e exibe o primeiro nome ou o botão de registro -->
+      <span v-if="isAuthenticated">
+        Welcome, {{ firstName }}
+      </span>
+      <v-btn text v-else @click="$router.push('/register')">Register</v-btn>
       <v-btn text v-if="!isAuthenticated" @click="$router.push('/login')">Login</v-btn>
       <v-btn text v-if="isAuthenticated" @click="logout">Logout</v-btn>
     </v-app-bar>
@@ -21,17 +25,26 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'App',
   computed: {
-    ...mapGetters(['isAuthenticated'])
+    ...mapGetters(['isAuthenticated', 'currentUser']),
+    
+    // Propriedade computada para extrair o primeiro nome do usuário
+    firstName() {
+      if (this.currentUser && this.currentUser.fullName) {
+        // Divide o nome completo em palavras e retorna o primeiro elemento
+        return this.currentUser.fullName.split(' ')[0];
+      }
+      return '';
+    }
   },
   methods: {
     ...mapActions(['logoutUser']),
     logout() {
       this.logoutUser();
-      this.$router.push('/'); // Redirect to home page after logout
+      this.$router.push('/'); // Redireciona para a página inicial após logout
     }
   },
   mounted() {
-    // Fetch payment methods when the app is mounted
+    // Buscar métodos de pagamento quando o app é montado
     this.$store.dispatch('fetchPaymentMethods');
   }
 };
